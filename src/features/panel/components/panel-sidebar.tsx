daf1988be38552f2db35fbc4bba02bdfa3eae5e1
avillis <caio.avillis@gmail.com>
2026-05-25 14:35:48 -0300

    Refine mobile burger icon proportions

--- src/features/panel/components/panel-sidebar.tsx
+++ src/features/panel/components/panel-sidebar.tsx
@@ -558,7 +558,8 @@ function BurgerIcon({ open }: { open: boolean }) {
     <div className="relative h-[18px] w-[18px]">
       <span
         className={cn(
-          "absolute left-0 top-1/2 block h-[2px] w-full -translate-y-1/2 rounded-full bg-current transition-transform duration-300 ease-out",
+          "absolute left-0 top-1/2 block h-[2px] -translate-y-1/2 rounded-full bg-current transition-[width,transform] duration-300 ease-out",
+          open ? "w-full" : "w-[70%]",
           open ? "rotate-45" : "-translate-y-[5px]",
         )}
       />
