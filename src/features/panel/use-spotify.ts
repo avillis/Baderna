c5c33c850a77e4842cddc1ab50247ece6dbdf354
avillis <caio.avillis@gmail.com>
2026-05-27 23:47:14 -0300

    fix: add topTracksRange to disconnect setData call

--- src/features/panel/use-spotify.ts
+++ src/features/panel/use-spotify.ts
@@ -64,7 +64,7 @@ export function useMySpotify() {
       method: "DELETE",
       headers: authHeaders(),
     });
-    setData({ connected: false, topTracks: [], recentlyPlayed: [] });
+    setData({ connected: false, topTracks: [], topTracksRange: "short", recentlyPlayed: [] });
   }, []);
 
   return { data, loading, connect, disconnect, reload: load };
